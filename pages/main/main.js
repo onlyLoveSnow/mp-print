bf634aaa225d30e0d21c5400a3fec6be3327a881
黎伟航 <lwh1094464365@163.com>
2019-11-20 11:20:38 +0800

    完善了打印预约功能

--- pages/main/main.js
+++ pages/main/main.js
@@ -5,69 +5,109 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    file: {
+      name: 'null',
+      path: 'null'
+    }
   },
 
-  print(){
-    console.log('hdh')
+  print() {
     wx.redirectTo({
       url: '/pages/print/print',
+    })
+  },
+
+  upload() {
+    var that = this;
+    wx.chooseMessageFile({
+      count: 1,
+      type: 'file',
+      success(res) {
+        var fileName = res.tempFiles[0].name;
+        var filePath = res.tempFiles[0].path;
+        that.setData({
+          ['file.name']: fileName,
+          ['file.path']: filePath
+        })
+        console.log(res);
+      }
+    })
+    wx.uploadFile({
+      url: 'http://192.168.1.100:8080/uploadFile',
+      filePath: that.data.file.path,
+      name: 'uploadFile',
+      success(res) {
+        var data = res.data
+        console.log(data)
+        if (data.status == 'success') {
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'none'
+          })
+        }
+      }
+    })
+  },
+
+  printQueue(){
+    wx.navigateTo({
+      url: '/pages/printList/printList',
     })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- pages/print/print.js
+++ pages/print/print.js
@@ -8,6 +8,11 @@ Page({
 
     array: ['A4', 'A3', '16开', '明信片'],
     index: 0,
+    file: {
+      name: '',
+      path: ''
+    },
+    user: null
 
   },
 
@@ -22,30 +27,80 @@ Page({
     console.log('form发生了submit事件，携带数据为：', e.detail.value)
     console.log(this.data.index)
 
-
-    wx.request({
-      url: 'http://127.0.0.1:8080/addPrint',
-      data: {
+    // wx.request({
+    //   url: 'http://127.0.0.1:8080/addPrint',
+    //   data: {
+    //     "size": this.data.array[this.data.index],
+    //     "printStyle": e.detail.value.option,
+    //     "printColor": e.detail.value.bw,
+    //     "notes": e.detail.value.input,
+    //     "number": wx.getStorageSync('user').num,
+    //   },
+    //   success(res) {
+    //     console.log(res.data)
+    //     if (res.data.status == 'success') {
+    //       console.log(res.data.msg)
+    //       wx.switchTab({
+    //         url: '/pages/main/main',
+    //       })
+    //       wx.showToast({
+    //         title: res.data.msg,
+    //         icon: 'none'
+    //       })
+    //     }
+    //   }
+    // })
+    var file = this.data.file 
+    console.log(file.path)
+    wx.uploadFile({
+      url: 'http://192.168.1.100:8080/addPrint',
+      filePath: file.path,
+      name: 'uploadFile',
+      formData: {
         "size": this.data.array[this.data.index],
         "printStyle": e.detail.value.option,
         "printColor": e.detail.value.bw,
         "notes": e.detail.value.input,
-        "number": wx.getStorageSync('user').number,
+        "number": wx.getStorageSync('user').num,
       },
       success(res) {
-        if (res.data.status == 'success') {
-          console.log(res.data.msg)
+        console.log(res.data)
+        var data = JSON.parse(res.data)
+        if (data.status == 'success') {
+          console.log(data.msg)
+
           wx.switchTab({
             url: '/pages/main/main',
           })
+          
           wx.showToast({
-            title: res.data.msg,
+            title: data.msg,
             icon: 'none'
           })
         }
       }
     })
+
   },
+
+  getFile() {
+    console.log('11')
+    wx.chooseMessageFile({
+      count: 1,
+      type: 'file',
+      success: (res) => {
+        var fileName = res.tempFiles[0].name;
+        var filePath = res.tempFiles[0].path;
+        this.setData({
+          ['file.name']: fileName,
+          ['file.path']: filePath
+        })
+      },
+    })
+    // console.log(res)
+
+  },
+
   formReset: function() {
     console.log('form发生了reset事件')
   },
